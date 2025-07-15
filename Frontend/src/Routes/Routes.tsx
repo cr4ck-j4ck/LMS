9da3cd5e37bfe5cf16ae273e9885b76c95c3863d
cr4ck-j4ck <vermapratyush486@gmail.com>
2025-07-15 13:27:59 +0530

    added Routes for the latest pages

--- Frontend/src/Routes/Routes.tsx
+++ Frontend/src/Routes/Routes.tsx
@@ -1,24 +1,27 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import Login from "../Auth/Login";
-import MainPage from "../pages/MainPage";
+import MainPage from "../pages/ShowLMS";
 import ShowClassroom from '../pages/ShowClassroom';
 import Moodle from '../pages/Moodle';
 import Canvas from '../pages/Canvas';
 import ReportsPage from '../pages/ReportsPage';
 import Welcome from '../pages/Welcome';
+import MoodleAboard from "@/pages/MoodleAboard";
+
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/" element={<Welcome />} />
+            <Route path="/showLMS" element={<MainPage />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/showClassroom" element={<ShowClassroom />} />
             <Route path="/moodle" element={<Moodle />} />
             <Route path="/canvas" element={<Canvas />} />
             <Route path="/ReportsPage" element={<ReportsPage/>} />
-            <Route path="/welcome" element={<Welcome />} />
+            
+            <Route path="/moodleAboard" element={<MoodleAboard/>} />
         </Routes>
     )
 };
